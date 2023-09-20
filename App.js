778e13914ccf4455bdd66adc0c6bbbfa47cde26e
daniel <felipe9321@hotmail.com>
2023-09-20 08:14:00 -0500

    loguin

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import { StyleSheet, Text, View } from 'react-native';
+import {styles} from './assets/styles/allstyles'
+import {LoginScreen} from './components/LoginScreen'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <LoginScreen/>
+    </View>
+  );
+}
+
---
+++ components/LoginScreen.js
@@ -0,0 +1,52 @@
+import {useState, useEffect} from 'react';
+import { View , Text } from 'react-native';
+import { TextInput , Avatar , Button } from 'react-native-paper';
+import {styles} from '../assets/styles/allstyles'
+
+export default function LoginScreen(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+    return(
+        <View style={styles.container}>
+            <Avatar.Image
+                style={{ marginBottom: 20 }}
+                size={100}
+                source={require('../assets/imgs/imglogin.png')} />
+            <View style={{ borderWidth: 2, borderColor: 'gray', borderRadius: 10, padding: 50 }}>
+                <TextInput
+                    autoFocus
+                    label="Correo Electrónico"
+                    left={<TextInput.Icon icon="email" />}
+                    onChangeText={(email) => setEmail(email)}
+                    value={email}
+                />
+                <TextInput
+                    style={{ marginTop: 20 }}
+                    label="Contraseña"
+                    secureTextEntry//={!showPass}
+                    onChangeText={(password) => setPassword(password)}
+                    value={password}
+                />
+                <Button
+                    style={{ marginTop: 20, backgroundColor: 'orange' }}
+                    icon="login"
+                    mode="outlined"
+                    onPress={handleSignIn}
+                >
+                    Iniciar Sesión
+                </Button>
+                <Button
+                    style={{ marginTop: 20, backgroundColor: 'yellow' }}
+                    icon="account"
+                    mode="outlined"
+                    //onPress={handleCreateAccount}
+                >
+                    Crear Cuenta
+                </Button>
+                <Text style={{color:'green'}}>{message}</Text>
+            </View>
+        </View>
+    )
+    
+}
